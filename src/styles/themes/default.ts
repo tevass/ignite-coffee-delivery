66d82db973b1c01ee9cd4fa61ec18436b436c844
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2022-08-20 12:02:05 -0300

    styles: defaultTheme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,64 @@
+export const defaultTheme = {
+  colors: {
+    base: {
+      white: '#FFFFFF',
+      background: '#FAFAFA',
+      card: '#F3F2F2',
+      input: '#EDEDED',
+      button: '#E6E5E5',
+      hover: '#D7D5D5',
+      label: '#8D8686',
+      text: '#574F4D',
+      subtitle: '#403937',
+      title: '#272221',
+    },
+    brand: {
+      purple: {
+        dark: '#4B2995',
+        normal: '#8047F8',
+        light: '#EBE5F9',
+      },
+      yellow: {
+        dark: '#C47F17',
+        normal: '#DBAC2C',
+        light: '#F1E9C9',
+      },
+    },
+  },
+  fonts: {
+    family: {
+      header: 'Baloo 2',
+      text: 'Roboto',
+    },
+    titles: {
+      xl: '3rem Baloo2 900 1.3',
+      lg: '2rem Baloo2 900 1.3',
+      md: '1.5rem Baloo2 900 1.3',
+      sm: '1.25rem Baloo2 bold 1.3',
+      xs: '1.125rem Baloo2 bold 1.3',
+    },
+    text: {
+      lg: {
+        regular: '1.25rem Roboto regular 1.3',
+        bold: '1.25rem Roboto bold 1.3',
+      },
+      md: {
+        regular: '1rem Roboto regular 1.3',
+        bold: '1rem Roboto bold 1.3',
+      },
+      sm: {
+        regular: '0.875rem Roboto regular 1.3',
+        bold: '0.875rem Roboto bold 1.3',
+      },
+    },
+    components: {
+      tag: {
+        bold: '0.625rem Roboto bold 1.3',
+      },
+      button: {
+        lg: '0.875rem Roboto bold 1.6',
+        sm: '0.75rem Roboto regular 1.6',
+      },
+    },
+  },
+}
